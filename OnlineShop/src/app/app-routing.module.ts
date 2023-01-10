7eb3872ed929c8696ee1550e70729e3cc1519ba0
JocaRistic <jocaristic5@gmail.com>
2023-01-11 00:21:26 +0100

    implementirana navigacija

--- OnlineShop/src/app/app-routing.module.ts
+++ OnlineShop/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PocetnaComponent } from './components/pocetna/pocetna.component';
+import { PonudaComponent } from './components/ponuda/ponuda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'pocetna', pathMatch:'full'},
+  {path: 'pocetna', component: PocetnaComponent},
+  {path: 'ponuda', component: PonudaComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- OnlineShop/src/app/app.module.ts
+++ OnlineShop/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PocetnaComponent } from './components/pocetna/pocetna.component';
+import { PonudaComponent } from './components/ponuda/ponuda.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PocetnaComponent,
+    PonudaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
